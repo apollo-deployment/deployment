8d1ccc05d1e053ef4390b1b7b9937894638677c2
vagrant <vagrant@homestead>
2018-03-31 06:25:02 +0000

    clean

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('/login', 'PageController@login')->name('view.login');
 
     // Authenticated routes
-    Route::group(['middleware' => ['auth']], function () {
+//    Route::group(['middleware' => ['auth']], function () {
         Route::get('/', 'PageController@deployment')->name('view.index');
         Route::get('/deployment', 'PageController@deployment')->name('view.deployment-plans');
         Route::get('/web-servers', 'PageController@webServers')->name('view.web-servers');
         Route::get('/projects', 'PageController@projects')->name('view.projects');
-    });
+//    });
 });
