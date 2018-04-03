3afef31311efa893f16d8cfcb0aab75aa83b7cc3
vagrant <vagrant@homestead>
2018-04-03 01:12:57 +0000

    saving

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Apollo\ApolloAPI;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    private $apollo;
+
+    public function __construct()
+    {
+        $this->apollo = new ApolloAPI;
+    }
+
+    /**
+     * Check API for existing user, if so log them in
+     */
+    public function login()
+    {
+        $data = $this->apollo->login(request());
+
+        if (isset($data->error)) {
+            return redirect()->back()->withInput()->withErrors(['login' => 'Incorrect username or password']);
+        }
+
+        Auth::attempt(['username' => $data->email]);
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 Route::group(['middleware' => ['web']], function () {
     Route::get('/login', 'PageController@login')->name('view.login');
+    Route::post('/login', 'Auth\AuthController@login')->name('login');
 
     // Authenticated routes
-//    Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['auth']], function () {
         Route::get('/', 'PageController@deployment')->name('view.index');
         Route::get('/deployment', 'PageController@deployment')->name('view.deployment-plans');
         Route::get('/web-servers', 'PageController@webServers')->name('view.web-servers');
         Route::get('/projects', 'PageController@projects')->name('view.projects');
-//    });
+    });
 });
