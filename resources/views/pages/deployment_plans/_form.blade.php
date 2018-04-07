8a78eced6bc442a9825dcaaa630b900009fbe678
zachary.sluder@gmail.com <vagrant@homestead>
2018-04-07 23:31:13 +0000

    labels

--- resources/views/pages/deployment_plans/_form.blade.php
+++ resources/views/pages/deployment_plans/_form.blade.php
@@ -6,13 +6,13 @@
 <div class="row">
     <div class="col-md-4">
         <div class="form-group">
-            <label for="name">Name</label>
+            {{ Form::label('name', 'Name') }}
             {{ Form::text('name', isset($plan) ? $plan->name : null, ['class' => 'form-control', 'required' => true]) }}
         </div>
     </div>
     <div class="col-md-4">
         <div class="form-group">
-            <label for="project_branch">Project Branch</label>
+            {{ Form::label('project_branch', 'Project Branch') }}
             {{ Form::text('project_branch', isset($plan) ? $plan->project_branch : null, ['class' => 'form-control', 'required' => true]) }}
         </div>
     </div>
@@ -24,13 +24,13 @@
 <div class="row">
     <div class="col-md-4">
         <div class="form-group">
-            <label for="project_id">Project</label>
+            {{ Form::label('project_id', 'Project') }}
             {{ Form::select('project_id', \App\Models\Project::pluck('name', 'id'), isset($plan) ? $plan->project->id : null, ['class' => 'form-control', 'required' => true]) }}
         </div>
     </div>
     <div class="col-md-4">
         <div class="form-group">
-            <label for="web_server_id">Web Server</label>
+            {{ Form::label('web_server_id', 'Web Server') }}
             {{ Form::select('web_server_id', \App\Models\WebServer::pluck('name', 'id'), isset($plan) ? $plan->webServer->id : null, ['class' => 'form-control', 'required' => true]) }}
         </div>
     </div>
@@ -39,13 +39,13 @@
 <div class="row">
     <div class="col-md-4">
         <div class="form-group">
-            <label for="storage_path">Code Storage Path</label>
+            {{ Form::label('storage_path', 'Code Storage Path') }}
             {{ Form::text('storage_path', isset($plan) ? $plan->storage_path : null, ['class' => 'form-control', 'required' => true]) }}
         </div>
     </div>
     <div class="col-md-4">
         <div class="form-group">
-            <label for="update_seconds">Update Seconds</label>
+            {{ Form::label('update_seconds', 'Update Seconds') }}
             {{ Form::number('update_seconds', isset($plan) ? $plan->update_seconds : 60, ['class' => 'form-control']) }}
         </div>
     </div>
--- resources/views/pages/projects/_form.blade.php
+++ resources/views/pages/projects/_form.blade.php
@@ -6,13 +6,13 @@
 <div class="row">
     <div class="col-md-6">
         <div class="form-group">
-            <label for="project_name">Project Name</label>
+            {{ Form::label('project_name', 'Project Name') }}
             {{ Form::text('project_name', isset($project) ? $project->name : null, ['class' => 'form-control', 'required' => true]) }}
         </div>
     </div>
     <div class="col-md-6">
         <div class="form-group">
-            <label for="repository_url">Repository URL</label>
+            {{ Form::label('repository_url', 'Repository URL') }}
             {{ Form::text('repository_url', isset($project) ? $project->repository_url : null, ['class' => 'form-control', 'required' => true]) }}
         </div>
     </div>
